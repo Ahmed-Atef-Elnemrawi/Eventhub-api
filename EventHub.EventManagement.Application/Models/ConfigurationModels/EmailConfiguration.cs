61c3997134500ed7f519a3649ded5dd723be4b89
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-01-08 03:56:51 +0200

    refactor email configuration model

--- EventHub.EventManagement.Application/Models/ConfigurationModels/EmailConfiguration.cs
+++ EventHub.EventManagement.Application/Models/ConfigurationModels/EmailConfiguration.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EventHub.EventManagement.Application.Models.ConfigurationModels
+﻿namespace EventHub.EventManagement.Application.Models.ConfigurationModels
 {
-   internal class EmailConfiguration
+#nullable disable
+   public class EmailConfiguration
    {
+      public string From { get; set; }
+      public string SmtpServer { get; set; }
+      public int Port { get; set; }
+      public string UserName { get; set; }
+      public string Password { get; set; }
    }
 }
