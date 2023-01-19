f51cc59e077f52d79c88c24db2f12d0d7599c227
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-01-20 00:57:45 +0200

    create change password Dto

---
+++ EventHub.EventManagement.Application/DTOs/UserDto/ChangePasswordDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EventHub.EventManagement.Application.DTOs.UserDto
+{
+   public record ChangePasswordDto
+   {
+      public string? CurrentPassword { get; init; }
+      public string? NewPassword { get; set; }
+   }
+}
