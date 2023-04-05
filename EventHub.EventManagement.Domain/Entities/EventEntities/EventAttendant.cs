ae7dafeaf2156e55420e9ce8cea586b850b20866
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-04-05 15:15:33 +0200

    -"feat: Add EventAttendant domain class"

---
+++ EventHub.EventManagement.Domain/Entities/EventEntities/EventAttendant.cs
@@ -0,0 +1,8 @@
+﻿namespace EventHub.EventManagement.Domain.Entities.EventEntities
+{
+   public class EventAttendant
+   {
+      public Guid EventId { get; set; }
+      public Guid AttendantId { get; set; }
+   }
+}
