5c30801bceb9e896a7d2708a9ee53af7657c5d45
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-04-05 14:56:17 +0200

    - feat: Create PagedListTypeEvent class for handling paged list type events

---
+++ EventHub.EventManagement.Application/RequestFeatures/Paging/PagedListTypeEvent.cs
@@ -0,0 +1,23 @@
+﻿using EventHub.EventManagement.Domain.Entities.EventEntities;
+
+namespace EventHub.EventManagement.Application.RequestFeatures.Paging
+{
+   public class PagedListTypeEvent<T> : List<T>
+      where T : Event
+   {
+      public MetaDataTypeEvent MetaData { get; set; }
+      public PagedListTypeEvent(List<T> items, int count, int upComingCount, int pageNumber, int pageSize)
+      {
+         MetaData = new MetaDataTypeEvent
+         {
+            TotalCount = count,
+            UpcomingCount = upComingCount,
+            PageSize = pageSize,
+            CurrentPage = pageNumber,
+            TotalPages = (int)Math.Ceiling(count / (double)pageSize)
+         };
+
+         AddRange(items);
+      }
+   }
+}
