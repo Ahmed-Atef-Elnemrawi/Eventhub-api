f01290988ce793aec3da074e91203f20ef33e73d
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-04-05 15:21:53 +0200

    "feat: Add UserPage domain class"

---
+++ EventHub.EventManagement.Domain/Entities/UserPage.cs
@@ -0,0 +1,15 @@
+﻿namespace EventHub.EventManagement.Domain.Entities
+{
+   public class UserPage
+   {
+      public Guid UserPageId { get; set; }
+      public PageType Type { get; set; }
+      public Guid EntityId { get; set; }
+   }
+
+   public enum PageType
+   {
+      producer,
+      organization
+   }
+}
