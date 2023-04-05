a6f33c2fe1ba7a4d4ba271fa84ae6cbd0bde8bfd
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-04-05 14:35:19 +0200

    - "Update EventDto for improved functionality "

--- EventHub.EventManagement.Application/DTOs/EventDto/EventDto.cs
+++ EventHub.EventManagement.Application/DTOs/EventDto/EventDto.cs
@@ -3,13 +3,16 @@
    public record EventDto
    {
       public Guid EventId { get; init; }
+      public Guid CategoryId { get; init; }
       public string? Name { get; init; }
       public DateTime Date { get; init; }
       public string? Description { get; init; }
+      public string? Discriminator { get; init; }
       public byte[]? Image { get; init; }
       public string? Url { get; init; }
       public string? City { get; init; }
       public string? Country { get; init; }
+      public string? EventState { get; init; }
 
    }
 }
