e75e87b36052205654984f1afc43a7d7a841a0b9
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-04-05 15:17:37 +0200

    -"feat: Add social media properties to Producer domain class"

--- EventHub.EventManagement.Domain/Entities/ProducerEntities/Producer.cs
+++ EventHub.EventManagement.Domain/Entities/ProducerEntities/Producer.cs
@@ -2,20 +2,21 @@
 
 namespace EventHub.EventManagement.Domain.Entities.ProducerEntities
 {
-
    public class Producer : ISortableEntity, ISearchableEntity
    {
       public Guid ProducerId { get; set; }
-      public Guid UserId { get; set; }
       public string? FirstName { get; set; }
       public string? LastName { get; set; }
       public Genre? Genre { get; set; }
       public int Age { get; set; }
-      public string? City { get; set; }
+      public string? LiveIn { get; set; }
       public string? PhoneNumber { get; set; }
       public string? Email { get; set; }
       public string? JobTitle { get; set; }
       public string? Bio { get; set; }
+      public string? Facebook { get; set; }
+      public string? Twitter { get; set; }
+      public string? LinkedIn { get; set; }
       public ICollection<Follower> Followers { get; set; }
       public ICollection<ProducerEvent> Events { get; set; }
 
