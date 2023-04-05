7297138430b9243571a7f93ca3852549f06982e3
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-04-05 14:45:44 +0200

    - feat: Create UserProfileForUpdateDto entity for updating user profiles

---
+++ EventHub.EventManagement.Application/DTOs/UserDto/UserProfileForUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using EventHub.EventManagement.Domain.Entities;
+
+namespace EventHub.EventManagement.Application.DTOs.UserDto
+{
+   public record class UserProfileForUpdateDto
+   {
+      public string? FirstName { get; init; }
+      public string? LastName { get; init; }
+      public string? UserName { get; init; }
+      public int Age { get; init; }
+      public string? Email { get; init; }
+      public string? PhoneNumber { get; init; }
+      public Genre Genre { get; init; }
+      public string? LiveIn { get; init; }
+      public byte[]? ProfilePicture { get; init; }
+      public UserPage? UserPage { get; init; }
+      public Guid? UserPageId { get; set; }
+   }
+}
