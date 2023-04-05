0bbab778df2ef3a9a89ba9ddf2d7ee1d9661b94f
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-04-05 14:33:48 +0200

    - "Update AttendantDto to improve functionality "

--- EventHub.EventManagement.Application/DTOs/AttendantDto/AttendantDto.cs
+++ EventHub.EventManagement.Application/DTOs/AttendantDto/AttendantDto.cs
@@ -1,16 +1,16 @@
-﻿namespace EventHub.EventManagement.Application.DTOs.AttendantDto
+﻿using EventHub.EventManagement.Domain.Entities;
+
+namespace EventHub.EventManagement.Application.DTOs.AttendantDto
 {
    public record AttendantDto
    {
-      public Guid AttendantId { get; set; }
+      public Guid AttendantId { get; init; }
       public string? FirstName { get; init; }
-      public string? LastName { get; set; }
-      public int Age { get; set; }
-      public string? Genre { get; set; }
-      public string? Phone { get; set; }
-      public string? Email { get; set; }
-      public string? LiveIn { get; set; }
-      public string? FullName { get; set; }
-      public string? Gender { get; set; }
+      public string? LastName { get; init; }
+      public Genre? Genre { get; init; }
+      public int Age { get; init; }
+      public string? City { get; init; }
+      public string? PhoneNumber { get; init; }
+      public string? Email { get; init; }
    }
 }
