c1c26a77ff5f70bdc31ed5a5c94d8d8f88524595
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-04-05 16:21:51 +0200

    feat: Add UserPage API controller

---
+++ EventHub.EventManagement.Presentation/Controllers/UserPageController.cs
@@ -0,0 +1,29 @@
+﻿using EventHub.EventManagement.Application.Contracts.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventHub.EventManagement.Presentation.Controllers
+{
+   [Route("api/v1.0/userpages")]
+   [ApiController]
+   public class UserPageController : ControllerBase
+   {
+      private readonly IServiceManager _service;
+
+      public UserPageController(IServiceManager service)
+      {
+         _service = service;
+      }
+
+      [HttpGet("{id:guid}", Name = "GetUserPage")]
+      public async Task<IActionResult> GetUserPageAsync([FromRoute] Guid id)
+      {
+         if (!ModelState.IsValid)
+            return NotFound(ModelState);
+
+         var userPage = await _service.UserPageService
+              .GetUserPageAsync(id, trackChanges: false);
+         return Ok(userPage);
+
+      }
+   }
+}
