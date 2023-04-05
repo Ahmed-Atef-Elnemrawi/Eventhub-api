d1667e92fb3e46a6c2caf85cf9fc8d283f913f05
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-04-05 14:41:51 +0200

    "Update ProducerDto for improved functionality"

--- EventHub.EventManagement.Application/DTOs/ProducerDto/ProducerForManipulationDto.cs
+++ EventHub.EventManagement.Application/DTOs/ProducerDto/ProducerForManipulationDto.cs
@@ -6,12 +6,15 @@ namespace EventHub.EventManagement.Application.DTOs.ProducerDto
    {
       public string? FirstName { get; init; }
       public string? LastName { get; init; }
-      public int Age { get; set; }
+      public int Age { get; init; }
       public Genre Genre { get; init; }
       public string? LiveIn { get; init; }
       public string? Email { get; init; }
       public string? PhoneNumber { get; init; }
       public string? JobTitle { get; init; }
       public string? Bio { get; init; }
+      public string? Facebook { get; init; }
+      public string? Twitter { get; init; }
+      public string? LinkedIn { get; init; }
    }
 }
