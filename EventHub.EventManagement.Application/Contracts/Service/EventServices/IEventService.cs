529d8c8a02b0696669d585d730f0391f9625c12f
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-05-22 00:17:07 +0300

    refactor: Update IEventService interface Rename GetAllCategoryEventsAsync method to GetAllCategoryProducerEventsAsync and change return type to Task<(LinkResponse linkresponse, MetaDataTypeEvent metaData)> Add GetAllProducersEventsAsync method with return type Task<(LinkResponse linkResponse, MetaDataTypeEvent metaData)> Remove GetCategoryEventAsync and GetEventAsync methods

--- EventHub.EventManagement.Application/Contracts/Service/EventServices/IEventService.cs
+++ EventHub.EventManagement.Application/Contracts/Service/EventServices/IEventService.cs
@@ -1,21 +1,19 @@
-﻿using EventHub.EventManagement.Application.Models;
-using EventHub.EventManagement.Application.Models.LinkModels;
+﻿using EventHub.EventManagement.Application.Models.LinkModels;
 using EventHub.EventManagement.Application.RequestFeatures.Paging;
-using EventHub.EventManagement.Application.RequestFeatures.Params;
 
 namespace EventHub.EventManagement.Application.Contracts.Service.EventServices
 {
    public interface IEventService
    {
-      Task<(LinkResponse linkResponse, MetaData metaData)>
-         GetAllCategoryEventsAsync(Guid mediumId, Guid categoryId, EventLinkParams linkParams, bool trackChanges);
+      Task<(LinkResponse linkresponse, MetaDataTypeEvent metaData)>
+         GetAllCategoryProducerEventsAsync(Guid mediumId, Guid categoryId, EventLinkParams linkParams, bool trackChanges);
 
-      Task<LinkResponse> GetCategoryEventAsync(Guid mediumId, Guid categoryId, Guid eventId, EventLinkParams linkParams, bool trackChanges);
+      //Task<LinkResponse> GetCategoryEventAsync(Guid mediumId, Guid categoryId, Guid eventId, EventLinkParams linkParams, bool trackChanges);
 
-      Task<ShapedEntity> GetEventAsync(Guid eventId, string fields, bool trackChanges);
+      //Task<ShapedEntity> GetEventAsync(Guid eventId, string fields, bool trackChanges);
 
-      Task<(IEnumerable<ShapedEntity> events, MetaData metaData)>
-         GetAllEventsAsync(EventParams eventParams, bool trackChanges);
+      Task<(LinkResponse linkResponse, MetaDataTypeEvent metaData)>
+         GetAllProducersEventsAsync(EventLinkParams linkParams, bool trackChanges);
 
    }
 }
