c811ab0def53596ce32e5719433aeea163520b0b
Ahmed Atef <ah.at.elnemrawi@gmail.com>
2023-05-22 00:57:58 +0300

    refactor: Change EventRepository class access modifier from internal to public

--- EventHub.EventManagement.Persistance/Repositories/EventRepositories/EventRepository.cs
+++ EventHub.EventManagement.Persistance/Repositories/EventRepositories/EventRepository.cs
@@ -2,7 +2,6 @@
 using EventHub.EventManagement.Application.RequestFeatures.Paging;
 using EventHub.EventManagement.Application.RequestFeatures.Params;
 using EventHub.EventManagement.Domain.Entities.EventEntities;
-using EventHub.EventManagement.Domain.Entities.OrganizationEntities;
 using EventHub.EventManagement.Domain.Entities.ProducerEntities;
 using EventHub.EventManagement.Persistance.Repositories.RepositoriesExtensions;
 using EventHub.EventManagement.Presistence;
@@ -11,40 +10,34 @@ using Microsoft.EntityFrameworkCore;
 
 namespace EventHub.EventManagement.Persistance.Repositories.EventRepositories
 {
-   internal sealed class EventRepository : BaseRepository<Event>, IEventRepository
+   public sealed class EventRepository : BaseRepository<Event>, IEventRepository
    {
       public EventRepository(RepositoryContext dbContext) : base(dbContext)
       {
       }
 
-      public async Task<PagedList<Event>> GetCategoryEventsAsync(Guid CategoryId,
+      public async Task<PagedListTypeEvent<ProducerEvent>> GetCategoryProducerEventsAsync(Guid CategoryId,
          EventParams eventParams, bool trackChanges)
       {
          var events =
               await FindByCondition(e => e.CategoryId.Equals(CategoryId),
               trackChanges)
+              .OfType<ProducerEvent>()
               .OrderBy(e => e.Name)
               .Skip((eventParams.PageNumber - 1) * eventParams.PageSize)
               .Take(eventParams.PageSize)
               .ToListAsync();
 
          var count = await FindByCondition(e => e.CategoryId.Equals(CategoryId), trackChanges).CountAsync();
+         var upComingCount = await FindByCondition(e =>
+            e.EventState.Equals(EventState.Upcoming), false).CountAsync();
 
-         return new PagedList<Event>
-            (events, count, eventParams.PageNumber, eventParams.PageSize);
+         return new PagedListTypeEvent<ProducerEvent>
+            (events, count, upComingCount, eventParams.PageNumber, eventParams.PageSize);
 
 
       }
 
-
-      public async Task<Event?> GetCategoryEventAsync(Guid categoryId, Guid eventId, bool trackChanges)
-      {
-         return
-            await FindByCondition(e => e.EventId.Equals(eventId)
-            && e.CategoryId.Equals(categoryId),
-            trackChanges)
-            .SingleOrDefaultAsync();
-      }
 
       public int GetEventsCount()
       {
@@ -53,19 +46,14 @@ namespace EventHub.EventManagement.Persistance.Repositories.EventRepositories
             .Count();
       }
 
-      public async Task<Event?> GetEventAsync(Guid eventId, bool trackChanges)
-      {
-         return
-            await FindByCondition(e =>
-            e.EventId.Equals(eventId),
-            trackChanges)
-            .SingleOrDefaultAsync();
-      }
-
-      public async Task<PagedList<Event>> GetAllEventsAsync(EventParams eventParams, bool trackChanges)
+      public async Task<PagedListTypeEvent<ProducerEvent>> GetAllproducersEventsAsync
+         (EventParams eventParams, bool trackChanges)
       {
          var events =
             await FindAll(trackChanges)
+            .OfType<ProducerEvent>()
+            .Include(e => e.Producer)
+            .Include(e => e.Category)
             .Filter(eventParams)
             .FilterByDate(eventParams)
             .Search(eventParams.SearchTerm)
@@ -73,32 +61,36 @@ namespace EventHub.EventManagement.Persistance.Repositories.EventRepositories
             .OrderBy(e => e.Name)
             .Skip((eventParams.PageNumber - 1) * eventParams.PageSize)
             .Take(eventParams.PageSize)
+            .OfType<ProducerEvent>()
             .ToListAsync();
 
          var count = await FindAll(trackChanges).CountAsync();
+         var upComingCount = await FindByCondition(e =>
+            e.EventState.Equals(EventState.Upcoming), false).CountAsync();
 
-         return new PagedList<Event>
-            (events, count, eventParams.PageNumber, eventParams.PageSize);
+         return new PagedListTypeEvent<ProducerEvent>
+            (events, count, upComingCount, eventParams.PageNumber, eventParams.PageSize);
 
       }
 
-      public async Task<ProducerEvent?> GetCategoryProducerEventAsync
-         (Guid categoryId, Guid eventId, bool trackChanges)
-      {
-         return await FindByCondition(e =>
-            e.CategoryId.Equals(categoryId) &&
-            e.EventId.Equals(eventId) &&
-            e.Discriminator!.Equals("ProducerEvent"), trackChanges).SingleOrDefaultAsync() as ProducerEvent;
-      }
+      //public async Task<ProducerEvent?> GetCategoryProducerEventAsync
+      //   (Guid categoryId, Guid eventId, bool trackChanges)
+      //{
+      //   return await FindByCondition(e =>
+      //      e.CategoryId.Equals(categoryId) &&
+      //      e.EventId.Equals(eventId) &&
+      //      e.Discriminator!.Equals("ProducerEvent"), trackChanges).Include(e => e.Producer)
+      //      .SingleOrDefaultAsync();
+      //}
 
-      public async Task<OrganizationEvent?> GetCategoryOrganizationEventAsync
-         (Guid categoryId, Guid eventId, bool trackChanges)
-      {
-         return await FindByCondition(e =>
-            e.CategoryId.Equals(categoryId) &&
-            e.EventId.Equals(eventId) &&
-            e.Discriminator!.Equals("OrganizationEvent"), trackChanges).SingleOrDefaultAsync() as OrganizationEvent;
-      }
+      //public async Task<OrganizationEvent?> GetCategoryOrganizationEventAsync
+      //   (Guid categoryId, Guid eventId, bool trackChanges)
+      //{
+      //   return await FindByCondition(e =>
+      //      e.CategoryId.Equals(categoryId) &&
+      //      e.EventId.Equals(eventId) &&
+      //      e.Discriminator!.Equals("OrganizationEvent"), trackChanges).SingleOrDefaultAsync() as OrganizationEvent;
+      //}
    }
 
 
